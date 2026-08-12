6e29cfa71213c2b7791c8121612c54f3c8e3a1a5
Priyash <priyashpatel13@gmail.com>
2026-08-13 00:32:15 +0530

    Updated the python versions for build

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 import requests
 import streamlit as st
 
-API_BASE = "https://movie-rec-466x.onrender.com" or "http://127.0.0.1:8000"
+API_BASE = "https://movie-recommendation-system-t50b.onrender.com" or "http://127.0.0.1:8000"
 TMDB_IMG = "https://image.tmdb.org/t/p/w500"
 
 st.set_page_config(page_title="Movie Recommender", page_icon="🎬", layout="wide")
